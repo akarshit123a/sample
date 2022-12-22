8c127245586df24038aec440cf5046ae98c1c41a
akarshit123a <70555660+akarshit123a@users.noreply.github.com>
2022-12-22 16:41:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,8 @@
-# This program prints Hello, world!
-
-print('Hello, world!')
+num1 = 15
+num2 = 12
+ 
+# Adding two nos
+sum = num1 + num2
+ 
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
